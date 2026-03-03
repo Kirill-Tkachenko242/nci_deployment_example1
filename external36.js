77668e1a0649e62891b64c3f0f78d6c66861b5a9
kirun225666 <110831941+Kirill-Tkachenko242@users.noreply.github.com>
2026-03-03 14:26:01 +0300

    Add files via upload

---
+++ external36.js
@@ -0,0 +1,28 @@
+function hello() {
+    var num1 = prompt("Enter a first number: ", "");
+    var num2 = prompt("Enter a second number: ", "");
+    var num1Int = parseInt(num1);
+    var num2Int = parseInt(num2);
+
+    if(num1Int<num2Int){
+        document.write("number 2 is bigger");
+    };
+    if(num1Int>num2Int){
+        document.write("number 1 is bigger");
+    };
+    if(num1Int==num2Int){
+        document.write("number 1 and 2 are similar");
+    };
+
+    var age = prompt("Enter your age: ", "");
+    var ageInt = parseInt(age);
+    if(ageInt>18){
+        document.write("<p> you are over than 18 </p>");
+    };
+    if(ageInt<18){
+        document.write("<p> you are under than 18 </p>");
+    };
+    if(ageInt==18){
+        document.write("<p> you are 18 years old </p>");
+    };
+}
